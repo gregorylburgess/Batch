591a8ca41b0053f6a5d5866a74d584e75824e9e4
gregorylburgess <gregory.l.burgess@gmailcom>
2014-12-10 18:25:46 -1000

    Added macro USERT to tell parser to use the real runtime if a dataset doesn't provide run time estimates.

--- Proc.h
+++ Proc.h
@@ -3,7 +3,13 @@
 #define WAITTIME_INDEX 2
 #define RUNTIME_INDEX 3
 #define NUMPROC_INDEX 4
-#define RUNTIMEESTIMATE_INDEX 8
+#ifdef USERT //Pass USERT as a parameter if the dataset doesn't give estimated runtimes
+	//If the dataset doesn't give estimated runtime, use real runtime
+	#define RUNTIMEESTIMATE_INDEX 3
+#else
+	//Otherwise, use the real runtime
+	#define RUNTIMEESTIMATE_INDEX 9
+#endif
 struct Proc {
 	int ID;
 	int numProc;
