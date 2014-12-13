0f1e8de8a3245078ccb72d2a11c32519a2ef7867
gregorylburgess <gregory.l.burgess@gmailcom>
2014-12-13 08:36:18 -1000

    Updated the run script to run all the new algorithms, run 20 iterations for random, averages for random, and print a nice csv.

--- run.sh
+++ run.sh
@@ -1,16 +1,72 @@
-numjobs=3000;
-for name in  "ctc.txt $numjobs 308" "cea.txt $numjobs 45824" "das.txt $numjobs 64" "nasa.txt $numjobs 128 test.txt $numjobs"
+numjobs=8000
+runs=20
+
+for name in  "ctc.txt" "cea.txt" "das.txt" "sdsc.txt" "nasa.txt"
 do
 echo "==========$name=========="
 echo "==========$name==========" >>"rslt.txt"
+let ts=0
+let as=0
+let ms=0
+let trt=0
+let np=0
 
-for alg in "FCFS" "BACKFILL" "SPIRAL"
+for alg in "FCFS" "BACKFILL" "SPIRAL" "EASY" "SPT" "LPT"
 do
+echo "$alg"
+g++ -D $alg -D SILENT -O4 Batch.cpp Proc.h Proc.cpp Slot.h Slot.cpp Event.h Event.cpp  -o batch
+rslt=$(./batch test/$name $numjobs)
+echo $rslt
+arrIN=(${rslt// / })
+#get stuff
+
+for x in $arrIN
+do
+	echo "> [$x]"
+done
+np=${arrIN[0]}
+ts=${arrIN[3]}
+as=${arrIN[6]}
+ms=${arrIN[9]}
+trt=${arrIN[13]}
+
+out="$np,$alg,$ts,$as,$ms,$trt"
+echo "$out"
+echo "$out" >> "rslt.txt"
+done
+
+#Random 20x
+alg="RANDOM"
 echo "$alg"
 echo "$alg" >> "rslt.txt"
 g++ -D $alg -D SILENT -O4 Batch.cpp Proc.h Proc.cpp Slot.h Slot.cpp Event.h Event.cpp  -o batch
-rslt=$(./batch test/$name)
-echo $rslt
-echo "$rslt" >> "rslt.txt"
+#stats for random
+let ts=0
+let as=0
+let ms=0
+let trt=0
+
+for j in `seq $runs`
+do
+#run random
+echo $j
+rslt=$(./batch test/$name $numjobs)
+arrIN=$(echo $rslt | tr ":" "\n")
+#sum stuff
+ts=`echo $ts+${arrIN[3]} | bc -l`
+as=`echo $as+${arrIN[6]} | bc -l`
+ms=`echo $ms+${arrIN[9]} | bc -l`
+trt=`echo $trt+${arrIN[13]} | bc -l`
 done
+
+# average
+ts=`echo $ts/$runs | bc -l`
+as=`echo $as/$runs | bc -l`
+ms=`echo $ms/$runs | bc -l`
+trt=`echo $trt/$runs | bc -l`
+
+out=",$np$alg,$ts,$as,$ms,$trt"
+echo "$out"
+echo "$out" >> "rslt.txt"
+
 done
